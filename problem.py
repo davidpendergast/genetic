d9e9ec2c6236926f4ddc5f03abba30dcd1e09f2d
davidpendergast <dlp75@case.edu>
2023-05-13 20:53:24 -0400

    it's working

--- problem.py
+++ problem.py
@@ -9,14 +9,26 @@ import math
 SIZE = (320, 240)
 INSET = 8
 
-N_BALLS = 15
-N_GOALS = 5
+N_BALLS = 4
+N_GOALS = 3
+N_SHOTS = 1
 
 BALL_RADIUS = 8
-IMPLUSE_FACTOR = 100
-FRICTION = 20
+IMPLUSE_FACTOR = 500
+FRICTION = 40
 
-MAX_SHOT_POWER = 75
+MAX_SHOT_POWER = 500
+
+SOLUTION_TIME_LIMIT = 15  # seconds
+TIME_STEP = 1 / 60
+
+POPULATION_SIZE = 32
+
+MUTATION_CHANCE = 0.25
+SWAP_CHANCE = 0.30
+LERP_CHANCE = 0.40
+NOISE_CHANCE_PER_IDX = 0.25
+NOISE_SIGMA = 0.25
 
 
 class Problem:
@@ -28,16 +40,43 @@ class Problem:
         self.goal_xys = goal_xys
         self.rect = rect
 
+        self.cached_solutions = {}
+
 @total_ordering
 class Solution:
     """Sequence of shots."""
 
     def __init__(self, data):
-        self.data = data
+        self.data = tuple(data)
         self.fitness = float('inf')
+
+    def create_new(self, data):
+        return Solution(data)
 
     def __lt__(self, other):
         return self.fitness < other.fitness
+
+    def swap(self, other, weight=0.5):
+        new_data = [(d1 if random.random() < weight else d2) for d1, d2 in zip(self.data, other.data)]
+        return self.create_new(new_data)
+
+    def lerp(self, other, weight=0.5):
+        new_data = [(d1 + weight * (d2 - d1)) for d1, d2 in zip(self.data, other.data)]
+        return self.create_new(new_data)
+
+    def jibble(self, amount):
+        new_data = [(d + 2 * (random.random() - 0.5) * amount) % 1.0 for d in self.data]
+        return self.create_new(new_data)
+
+    def __eq__(self, other):
+        return self.data == other.data
+
+    def __hash__(self):
+        return hash(self.data)
+
+    def __repr__(self):
+        datas = ", ".join(tuple(f'{d:.3f}' for d in self.data))
+        return f"S({datas}) = {self.fitness:.1f}"
 
     @classmethod
     def create_random_data(cls, size):
@@ -47,6 +86,9 @@ class ShotSequenceSolution(Solution):
 
     def __init__(self, data):
         super().__init__(data)
+
+    def create_new(self, data):
+        return ShotSequenceSolution(data)
 
     def num_shots(self):
         return len(self.data) // 2
@@ -58,7 +100,7 @@ class ShotSequenceSolution(Solution):
             angle = self.data[idx // 2]
             power = self.data[idx // 2 + 1]
             vec = pygame.Vector2()
-            vec.from_polar((angle * 360, power * MAX_SHOT_POWER))
+            vec.from_polar((power * MAX_SHOT_POWER, angle * 360))
             return vec
 
     @classmethod
@@ -111,12 +153,12 @@ class Ball:
 class ProblemSolver:
 
     def __init__(self, problem: Problem, solution: ShotSequenceSolution):
-        # initial conditions
         self.problem = problem
         self.solution = solution
-        self.t_limit = 15  # seconds
+        self.t_limit = SOLUTION_TIME_LIMIT
 
         # actual state of the simulation
+        self.done = False
         self.shot_idx = -1
         self.t = 0
         self.goal_xys = [pygame.Vector2(gxy) for gxy in problem.goal_xys]
@@ -126,6 +168,9 @@ class ProblemSolver:
     def update(self, dt):
         self.t += dt
 
+        if self.t > self.t_limit:
+            self.done = True
+
         if self.shot_idx == -1 or self.is_everything_stationary():
             if self.shot_idx < self.solution.num_shots() - 1:
                 self.shot_idx += 1
@@ -133,7 +178,9 @@ class ProblemSolver:
                 self.cue_ball.vel.x = next_shot.x
                 self.cue_ball.vel.y = next_shot.y
             else:
-                self.solution.fitness = self.get_fitness()
+                self.done = True
+                if self.solution.fitness == float('inf'):
+                    self.solution.fitness = self.get_fitness()
 
         # simulate movement
         for b in self.all_balls():
@@ -184,17 +231,14 @@ class ProblemSolver:
         return all([b.vel.magnitude() <= 0.0001 for b in self.all_balls()])
 
     def is_done(self):
-        return self.solution.fitness != float('inf')
+        return self.done
 
     def get_fitness(self):
-        if self.solution.fitness != float('inf'):
-            return self.solution.fitness
-        else:
-            total = 0
-            for ball in self.all_balls():
-                if not ball.is_cue:
-                    total += self.get_dist_to_nearest_goal(ball.xy)
-            return total
+        total = 0
+        for ball in self.all_balls():
+            if not ball.is_cue:
+                total += self.get_dist_to_nearest_goal(ball.xy)
+        return total
 
 
 class AnimatedProblemSolver(ProblemSolver):
@@ -212,7 +256,7 @@ class AnimatedProblemSolver(ProblemSolver):
         self.cue_ball.draw(screen, self.cue_ball.xy)
 
         for target_ball in self.balls:
-            fitness = self.get_dist_to_nearest_goal(target_ball.xy)
+            fitness = self.get_dist_to_nearest_goal(target_ball.xy) + 1
             color = pygame.Color("green").lerp("red", min(1, (fitness / (self.problem.rect.width // 3))))
             target_ball.draw(screen, target_ball.xy, color=color)
 
@@ -227,7 +271,7 @@ def point_dist(p1, p2):
     dy = p1[1] - p2[1]
     return math.sqrt(dx * dx + dy * dy)
 
-def create_sample_problem():
+def create_sample_problem() -> Problem:
     playing_rect = pygame.Rect(INSET, INSET, SIZE[0] - INSET * 2, SIZE[1] - INSET * 2)
     spawn_rect = playing_rect.inflate(-BALL_RADIUS * 2, -BALL_RADIUS * 2)
 
@@ -244,10 +288,81 @@ def create_sample_problem():
 
     goals = [rand_point_in_rect(spawn_rect) for _ in range(N_GOALS)]
 
-    sample_problem = Problem(balls, goals, playing_rect)
-    sample_solution = ShotSequenceSolution.create_random(3)
+    return Problem(balls, goals, playing_rect)
 
-    return AnimatedProblemSolver(sample_problem, sample_solution)
+
+class SolutionManager:
+
+    def __init__(self, problem,
+                 solution_generator,
+                 pop_size=POPULATION_SIZE,
+                 mutation_rate=MUTATION_CHANCE,
+                 swap_rate=SWAP_CHANCE,
+                 preserve_best=False):
+        self.problem = problem
+        self.solution_generator = solution_generator
+
+        self.size = pop_size
+        self.mutation_rate = mutation_rate
+        self.swap_rate = swap_rate
+        self.preserve_best = preserve_best
+
+        self.generations = []
+
+    def get_current_generation(self):
+        return self.generations[-1]
+
+    def get_best_solution(self):
+        return self.get_current_generation()[0]
+
+    def evaluate(self, solution) -> Solution:
+        if solution in self.problem.cached_solutions:
+            solution.fitness = self.problem.cached_solutions[solution]
+        else:
+            t = 0
+            solver = ProblemSolver(self.problem, solution)
+            while t < SOLUTION_TIME_LIMIT and not solver.is_done():
+                solver.update(TIME_STEP)
+                t += TIME_STEP
+            solution.fitness = solver.get_fitness()
+        return solution
+
+    def create_next_generation(self):
+        if len(self.generations) == 0:
+            next_gen = [self.evaluate(self.solution_generator()) for _ in range(self.size)]
+        else:
+            cur_gen = self.get_current_generation()
+            weights = [1 / s.fitness for s in cur_gen]
+
+            next_gen = []
+            while len(next_gen) < self.size:
+                rng = random.random()
+
+                if rng < MUTATION_CHANCE:
+                    new_solution = self.solution_generator()
+                else:
+                    s1, s2 = random.choices(cur_gen, weights, k=2)
+                    s3 = self.evaluate(s1.lerp(s2, weight=0.333 + 0.333 * random.random()))
+                    if s3.fitness <= min(s1.fitness, s2.fitness):
+                        new_solution = s3
+                    else:
+                        new_solution = min(s1, s2)
+
+                if new_solution in next_gen:
+                    new_solution = new_solution.jibble(0.25)
+
+                next_gen.append(self.evaluate(new_solution))
+
+        next_gen.sort()
+
+        fits = [f"{s.fitness:.1f}" for s in next_gen]
+        print(f"GEN {len(self.generations) + 1}: {fits}")
+
+        return next_gen
+
+    def step(self) -> Solution:
+        self.generations.append(self.create_next_generation())
+        return self.get_best_solution()
 
 
 if __name__ == "__main__":
@@ -257,7 +372,10 @@ if __name__ == "__main__":
     clock = pygame.Clock()
     dt = 0
 
-    solver = create_sample_problem()
+    problem = create_sample_problem()
+    solver = SolutionManager(problem, lambda: ShotSequenceSolution.create_random(N_SHOTS))
+
+    animated_solver = None
 
     font = pygame.Font(None, size=16)
 
@@ -270,21 +388,31 @@ if __name__ == "__main__":
                 if e.key == pygame.K_ESCAPE:
                     running = False
                 elif e.key == pygame.K_r:
-                    solver = create_sample_problem()
+                    problem = create_sample_problem()
+                    solver = SolutionManager(problem, lambda: ShotSequenceSolution.create_random(N_SHOTS))
+                    animated_solver = None
 
-        solver.update(1 / 60)  # fixed timestep for consistency's sake
-        solver.draw(screen)
+        if animated_solver is None or animated_solver.is_done():
+            last_best = None if animated_solver is None else animated_solver.solution.fitness
+            next_solution = solver.step()
+            animated_solver = AnimatedProblemSolver(problem, next_solution)
+            if last_best is not None and next_solution.fitness >= last_best:
+                animated_solver.done = True  # skip it
+
+        animated_solver.update(1 / 60)  # fixed timestep for consistency's sake
+        animated_solver.draw(screen)
+
+        text_to_render = [
+            (f"Generation: {len(solver.generations)}", "snow"),
+            (f"Solution: {solver.get_best_solution()}", "snow"),
+            (f"Shot: {animated_solver.shot_idx + 1}, Fitness: {animated_solver.get_fitness():.1f}", "snow")
+        ]
 
         text_y = INSET + 2
-        fitness_text = f"Fitness: {solver.get_fitness():.2f}"
-        fitness_surf = font.render(fitness_text, True, "snow" if not solver.is_done() else "yellow")
-        screen.blit(fitness_surf, (INSET + 2, text_y))
-        text_y += fitness_surf.get_height() + 2
-
-        shot_text = f"Shot: {solver.shot_idx + 1}"
-        shot_surf = font.render(shot_text, True, "snow")
-        screen.blit(shot_surf, (INSET + 2, text_y))
-        text_y += shot_surf.get_height() + 2
+        for text, color in text_to_render:
+            surf = font.render(text, True, color)
+            screen.blit(surf, (INSET + 2, text_y))
+            text_y += surf.get_height() + 2
 
         dt = clock.tick(60) / 1000
         pygame.display.flip()
