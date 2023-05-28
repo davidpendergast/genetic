0c7c24827315edd2a3059c5a1624adc4ccce183b
davidpendergast <dlp75@case.edu>
2023-05-28 14:06:17 -0400

    added new, better rendering method using euler angles

--- balls.py
+++ balls.py
@@ -5,6 +5,7 @@ import pygame
 
 import rainbowize
 import utils.qutils as qutils
+import utils.profiling as profiling
 
 
 TEXTURES = {}
@@ -35,82 +36,112 @@ def fibonacci_sphere(samples=64):
 
 IMG_CACHE = {}
 
+def quantize(val, max_val, n, and_round=False, wrap=True):
+    if wrap:
+        quant = round((val % max_val) / (max_val / n)) * (max_val / n)
+        return round(quant) if and_round else quant
+    else:
+        raise NotImplementedError()
+
 class Ball:
 
-    def __init__(self, radius, pos=(0, 0), res=8, texture_id='default', orient=None):
+    def __init__(self, radius, pos=(0, 0), res=8, texture_id='default', mode='fib', orient=None):
         self.radius = radius
         self.pos = pygame.Vector3(pos[0], pos[1], 0)
         self.texture_id = texture_id
-        self.orient = orient or qutils.Qk
+        self.orient = orient or qutils.get_rot_quat((0, 0, 1), 0)
         self.outline = ("white", 1)
         self.res = res
-        self.mode = 'fib'
-
-        self._cache = {}
+        self.mode = mode
 
     def get_normalized_orient(self):
+        orient = self.orient if self.orient[1] >= 0 else qutils.qscale(self.orient, 1)
+        as_vec, angle_rads = qutils.qdecomp(orient)
+        as_vec = pygame.Vector3(as_vec)
+        angle = angle_rads * 180 / math.pi
         if self.res <= 0:
-            as_vec, angle = qutils.qdecomp(self.orient)
-            return pygame.Vector3(as_vec), angle
+            return as_vec, angle
         elif self.mode == 'fib':
-            as_vec, angle = qutils.qdecomp(self.orient)
-            as_vec = pygame.Vector3(as_vec)
             fib_vecs = fibonacci_sphere(samples=self.res)
-            new_angle = round((angle % (2 * math.pi)) / (2 * math.pi / self.res)) * (2 * math.pi / self.res)
+            new_angle = round((angle % 360) / (360 / self.res)) * (360 / self.res)
             to_use = min(fib_vecs, key=lambda v: as_vec.distance_squared_to(v))
-            # print(f"{as_vec=} {angle=:.2f} --> {to_use=} {new_angle=:.2f}")
             return pygame.Vector3(to_use), new_angle
         else:
-            as_vec, angle = qutils.qdecomp(self.orient)
-            as_vec = pygame.Vector3(as_vec)
             (r, theta, phi) = as_vec.as_spherical()
             theta = round(self.res * theta / 180) * 180 // self.res
             phi = round(self.res * phi / 360) * 360 // self.res
-            angle = round(self.res * angle / (2 * math.pi)) * (2 * math.pi) // self.res
+            angle = round((angle % 360) / (360 / self.res)) * (360 / self.res)
             norm_vec = pygame.Vector3()
             norm_vec.from_spherical((1, theta, phi))
             return norm_vec, angle
 
     def get_img(self):
-        vec, angle = self.get_normalized_orient()
-        key = (tuple(vec), angle, self.outline, self.texture_id, self.radius)
-        if key not in IMG_CACHE:
-            # print(f"new key: {key}")
+        if self.mode == 'eul':
+            x, y, z = qutils.to_euler2(self.orient)
+            quant_x = quantize(x * 180 / math.pi, 360, int(math.sqrt(self.res)), and_round=True)
+            quant_y = quantize(y * 180 / math.pi, 360, int(math.sqrt(self.res)), and_round=True)
+            rot_z = -z * 180 / math.pi
+
+            key = (quant_x, quant_y, self.texture_id, self.radius)
+            if key not in IMG_CACHE:
+                orient = qutils.from_euler2(quant_x / 180 * math.pi, quant_y / 180 * math.pi, 0)
+                # print(f"({self.orient[0]:.1f}, {self.orient[1]:.1f}, {self.orient[2]:.1f}, {self.orient[3]:.1f})=orient, "
+                #       f"({x:.1f}, {y:.1f}, {z:.1f})=xyz, "
+                #       f"({quant_x:.1f}, {quant_y:.1f}, {rot_z:.1f})=(quant_x, quant_y, rot_z) -->"
+                #       f"({orient[0]:.1f}, {orient[1]:.1f}, {orient[2]:.1f}, {orient[3]:.1f})")
+                IMG_CACHE[key] = self._render(orient, ('white', 0))
+            img = IMG_CACHE[key]
+
+            img_rot = pygame.transform.rotate(img, rot_z)
             out_color, out_width = self.outline
-            res = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
-            orient = qutils.get_rot_quat(vec, angle)
+            if out_width > 0:
+                pygame.draw.circle(img_rot, out_color, img_rot.get_rect().center, self.radius, width=out_width)
 
-            surf_center = pygame.Vector2(res.get_width() / 2, res.get_height() / 2)
-            texture = TEXTURES[self.texture_id]
+            return img_rot
 
-            view_dir = pygame.Vector3(0, 0, -1)
-            for y in range(res.get_height()):
-                for x in range(res.get_width()):
-                    if surf_center.distance_to((x + 0.5, y + 0.5)) < self.radius - 0.333:
-                        R = self.radius
-                        V, X = view_dir, pygame.Vector3(x + 0.5 - surf_center.x, y + 0.5 - surf_center.y, self.radius)
-                        X2, V2 = X.magnitude_squared(), V.magnitude_squared()
-                        t = (-2 * V.dot(X) - math.sqrt((2 * V.dot(X))**2 - 4 * V2 * (X2 - R*R))) / (2 * V2)
+        else:
+            vec, angle = self.get_normalized_orient()
+            key = (tuple(round(x, 3) for x in vec), round(angle, 3), self.outline, self.texture_id, self.radius)
+            if key not in IMG_CACHE:
+                orient = qutils.get_rot_quat(vec, angle * math.pi / 180)
+                img = self._render(orient, self.outline)
 
-                        pt_on_sphere = X + t * V
-                        pt_on_sphere = pygame.Vector3(qutils.rotate_pt_about_quat(pt_on_sphere, orient))
-                        (r, theta, phi) = pt_on_sphere.as_spherical()
-                        tx_coords = (int(phi % 360 * texture.get_width() / 360),
-                                     int(theta / 180 * texture.get_height()))
+                IMG_CACHE[key] = img
+            return IMG_CACHE[key]
 
-                        # if (x, y) == (res.get_width() // 2, res.get_height() // 2):
-                        #     print(f"{pt_on_sphere=}, {(r, theta, phi)=}, {tx_coords=}")
-                        # print(f"{(x,y)=}, {pt_on_sphere}, {r=}, {theta=}, {phi=}, {tx_coords=}")
+    def _render(self, orient, outline):
+        out_color, out_width = outline
+        res = pygame.Surface((self.radius * 2, self.radius * 2), pygame.SRCALPHA)
+
+        surf_center = pygame.Vector2(res.get_width() / 2, res.get_height() / 2)
+        texture = TEXTURES[self.texture_id]
+
+        view_dir = pygame.Vector3(0, 0, -1)
+        for y in range(res.get_height()):
+            for x in range(res.get_width()):
+                if surf_center.distance_to((x + 0.5, y + 0.5)) < self.radius - 0.333:
+                    R = self.radius
+                    V, X = view_dir, pygame.Vector3(x + 0.5 - surf_center.x, y + 0.5 - surf_center.y, self.radius)
+                    X2, V2 = X.magnitude_squared(), V.magnitude_squared()
+                    t = (-2 * V.dot(X) - math.sqrt((2 * V.dot(X)) ** 2 - 4 * V2 * (X2 - R * R))) / (2 * V2)
+
+                    pt_on_sphere = X + t * V
+                    pt_on_sphere = pygame.Vector3(qutils.rotate_pt_about_quat(pt_on_sphere, orient))
+                    (r, theta, phi) = pt_on_sphere.as_spherical()
+                    tx_coords = (int(phi % 360 * texture.get_width() / 360),
+                                 int(theta / 180 * texture.get_height()))
+
+                    try:
                         base_color = texture.get_at(tx_coords)
                         shaded_color = pygame.Color(base_color).lerp("black", t / R * 0.666)
                         res.set_at((x, y), shaded_color)
+                    except IndexError:
+                        pass
 
-            if out_width > 0:
-                pygame.draw.circle(res, out_color, surf_center, self.radius, width=out_width)
+        if out_width > 0:
+            pygame.draw.circle(res, out_color, surf_center, self.radius, width=out_width)
 
-            IMG_CACHE[key] = res
-
-        return IMG_CACHE[key]
+        return res
 
     def move(self, vec3, rotate=True):
         self.pos += vec3
@@ -131,6 +162,7 @@ if __name__ == "__main__":
     SIZE = (256, 256)
     screen = rainbowize.make_fancy_scaled_display(SIZE, scale_factor=3, extra_flags=pygame.RESIZABLE)
     clock = pygame.Clock()
+    dt = 0
 
     TEXTURES['default'] = pygame.image.load("assets/rainbow_skulls.png")
 
@@ -139,7 +171,7 @@ if __name__ == "__main__":
     balls = []
     for x in range(grid_res):
         for y in range(grid_res):
-            b = Ball(8)
+            b = Ball(8, res=16)
             b.pos = pygame.Vector3((x + 0.5) * SIZE[0] // grid_res, (y + 0.5) * SIZE[1] // grid_res, 0)
             rot_axis = pygame.Vector2(1, 0).rotate(x * 360 / grid_res)
             rot_amt = y * 180 / grid_res
@@ -149,8 +181,9 @@ if __name__ == "__main__":
             balls.append(b)
 
     movable_balls = [
-        Ball(16, pos=(SIZE[0] / 2, SIZE[1] / 2), res=0),
-        Ball(16, pos=(SIZE[0] / 2 + 48, SIZE[1] / 2), res=128)
+        # Ball(16, pos=(SIZE[0] / 2, SIZE[1] / 2), res=0),
+        # Ball(16, pos=(SIZE[0] / 2 + 48, SIZE[1] / 2), res=128, mode='fib'),
+        Ball(16, pos=(SIZE[0] / 2 + 48 * 2, SIZE[1] / 2), res=256, mode='eul')
     ]
 
     speed = 150
@@ -160,13 +193,16 @@ if __name__ == "__main__":
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 running = False
+            elif e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_F1:
+                    profiling.get_instance().toggle()
         keys = pygame.key.get_pressed()
 
         dx = keys[pygame.K_d] - keys[pygame.K_a]
         dy = keys[pygame.K_s] - keys[pygame.K_w]
         if dx != 0 or dy != 0:
             move = pygame.Vector3(dx, dy, 0)
-            move.scale_to_length(speed / 60)
+            move.scale_to_length(speed * dt)
             for b in movable_balls:
                 b.move(move)
 
@@ -177,13 +213,13 @@ if __name__ == "__main__":
 
         screen.fill("darkgray")
 
-        for b in balls:
-            b.render(screen, (b.pos.x, b.pos.y))
+        # for b in balls:
+        #     b.render(screen, (b.pos.x, b.pos.y))
 
         for b in movable_balls:
             b.render(screen, (b.pos.x, b.pos.y))
 
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
--- utils/qutils.py
+++ utils/qutils.py
@@ -2,13 +2,19 @@ import math
 import typing
 import random
 
+import pygame
+
+Tuple3f = typing.Tuple[float, float, float]
+Vector3Like = typing.Union[Tuple3f, pygame.Vector3]
+QuaternionType = typing.Tuple[float, float, float, float]
+QuaternionLike = typing.Union[float, int, QuaternionType, Vector3Like]
 
 Qi = (0, 1, 0, 0)
 Qj = (0, 0, 1, 0)
 Qk = (0, 0, 0, 1)
 
 
-def quat(q: typing.Union[float, int, tuple]) -> typing.Tuple:
+def quat(q: QuaternionLike) -> QuaternionType:
     if isinstance(q, (float, int)):
         return (q, 0, 0, 0)
     elif len(q) == 4:
@@ -21,7 +27,7 @@ def quat(q: typing.Union[float, int, tuple]) -> typing.Tuple:
         raise ValueError(f"invalid input: {q}")
 
 
-def qmult(q, p):
+def qmult(q: QuaternionLike, p: QuaternionLike) -> QuaternionType:
     a1, b1, c1, d1 = quat(q)
     a2, b2, c2, d2 = quat(p)
     return (
@@ -31,46 +37,50 @@ def qmult(q, p):
         a1*d2 + b1*c2 - c1*b2 + d1*a2
     )
 
+def qscale(q: QuaternionLike, a: float) -> QuaternionType:
+    a1, b1, c1, d1 = quat(q)
+    return (a1*a, b1*a, c1*a, d1*a)
 
-def qprod(*qs):
+
+def qprod(*qs: QuaternionLike) -> QuaternionType:
     res = (1, 0, 0, 0)
     for q in qs:
         res = qmult(res, q)
     return res
 
 
-def qadd(q, p):
+def qadd(q: QuaternionLike, p: QuaternionLike) -> QuaternionType:
     a1, b1, c1, d1 = quat(q)
     a2, b2, c2, d2 = quat(p)
     return (a1 + a2, b1 + b2, c1 + c2, d1 + d2)
 
 
-def qsum(*qs):
+def qsum(*qs: QuaternionLike) -> QuaternionType:
     res = (0, 0, 0, 0)
     for q in qs:
         res = qadd(res, q)
     return res
 
 
-def qconj(q):
+def qconj(q: QuaternionLike) -> QuaternionType:
     a, b, c, d = quat(q)
     return (a, -b, -c, -d)
 
 
-def qnorm(q):
+def qnorm(q: QuaternionLike) -> float:
     a, b, c, d = quat(q)
     return math.sqrt(a*a + b*b + c*c + d*d)
 
 
-def qnormalize(q, to_len=1):
+def qnormalize(q: QuaternionLike, to_len=1) -> QuaternionType:
     return qmult(q, to_len / qnorm(q))
 
 
-def qrecip(q):
+def qrecip(q: QuaternionLike) -> QuaternionType:
     return qmult(qconj(q), 1 / qnorm(q)**2)
 
 
-def qpow(q, t):
+def qpow(q: QuaternionLike, t: float) -> QuaternionType:
     a, b, c, d = quat(q)
     if b == 0 and c == 0 and d == 0:
         return (a ** t, 0, 0, 0)
@@ -78,7 +88,7 @@ def qpow(q, t):
         return qexp(qmult(t, qlog(q)))
 
 
-def qexp(q):
+def qexp(q: QuaternionLike) -> QuaternionType:
     a, b, c, d = quat(q)
     ea = math.e ** a
     theta = math.sqrt(b*b + c*c + d*d)
@@ -89,7 +99,7 @@ def qexp(q):
             ea * d * s)
 
 
-def qlog(q):
+def qlog(q: QuaternionLike) -> QuaternionType:
     a, b, c, d = quat(q)
     v_norm = math.sqrt(b*b + c*c + d*d)
     q_norm = qnorm(q)
@@ -98,7 +108,7 @@ def qlog(q):
     return (math.log(q_norm), val * b, val * c, val * d)
 
 
-def qlerp(q, p, t):
+def qlerp(q: QuaternionLike, p: QuaternionLike, t: float) -> QuaternionType:
     a1, b1, c1, d1 = quat(q)
     a2, b2, c2, d2 = quat(p)
     return (a1 + t * (a2 - a1),
@@ -107,12 +117,12 @@ def qlerp(q, p, t):
             d1 + t * (d2 - d1))
 
 
-def qslerp(q, p, t):
+def qslerp(q: QuaternionLike, p: QuaternionLike, t: float) -> QuaternionType:
     q_r = qrecip(q)
     return qmult(q, qpow(qmult(q_r, p), t))
 
 
-def qdecomp(q):
+def qdecomp(q: QuaternionLike) -> typing.Tuple[Tuple3f, float]:
     a, b, c, d = quat(q)
     m = math.sqrt(b*b + c*c + d*d)
     v = (b / m, c / m, d / m) if m != 0 else (0, 0, 1)
@@ -120,16 +130,16 @@ def qdecomp(q):
     return v, theta
 
 
-def get_rot_quat(v_3d, theta):
+def get_rot_quat(v_3d: Vector3Like, theta: float) -> QuaternionType:
     return qadd(math.cos(theta / 2), qmult(math.sin(theta / 2), qnormalize(v_3d)))
 
 
-def rotate_pt_about_quat(pt_3d, q):
+def rotate_pt_about_quat(pt_3d: Vector3Like, q: QuaternionLike):
     pt_rot = qprod(q, pt_3d, qrecip(q))
     return pt_rot[1:4]
 
 
-def rotate_pt_about_vec(pt_3d, v_3d, theta):
+def rotate_pt_about_vec(pt_3d: Vector3Like, v_3d: Vector3Like, theta: float):
     q = get_rot_quat(v_3d, theta)
     return rotate_pt_about_quat(pt_3d, q)
 
@@ -152,3 +162,87 @@ def rand_vec(dim):
         return (1,) + (0,) * (dim - 1)
     else:
         return tuple(v / m for v in vals)
+
+
+def to_euler2(q):
+    qw, qx, qy, qz = q
+    qprime = (-qw, qz, qy, qx)
+    zyx = to_euler_321(qprime)
+    return zyx[2], zyx[1], zyx[0]
+
+
+def from_euler2(roll_x, pitch_y, yaw_z):
+    zyx = (yaw_z, pitch_y, roll_x)
+    qw, qx, qy, qz = from_euler_321(*zyx)
+    return (-qw, qz, qy, qx)
+
+
+def to_euler(q: QuaternionLike) -> Tuple3f:
+    """
+    :return: roll (x), pitch (y), yaw (z) [123 order]
+    """
+    return euler_321_to_123(to_euler_321(q))
+
+def from_euler(roll_x: float, pitch_y: float, yaw_z: float) -> QuaternionType:
+    return (from_euler_321(*euler_123_to_321((roll_x, pitch_y, yaw_z))))
+
+def to_euler_321(q: QuaternionLike) -> Tuple3f:
+    """
+    :return: roll (x), pitch (y), yaw (z)
+    """
+    # roll(x - axis rotation)
+    sinr_cosp = 2 * (q[0] * q[1] + q[2] * q[3])
+    cosr_cosp = 1 - 2 * (q[1] * q[1] + q[2] * q[2])
+    roll_x = math.atan2(sinr_cosp, cosr_cosp)
+
+    # pitch(y - axis rotation)
+    sinp = math.sqrt(1 + 2 * (q[0] * q[2] - q[1] * q[3]))
+    cosp = math.sqrt(1 - 2 * (q[0] * q[2] - q[1] * q[3]))
+    pitch_y = 2 * math.atan2(sinp, cosp) - math.pi / 2
+
+    # yaw(z - axis rotation)
+    siny_cosp = 2 * (q[0] * q[3] + q[1] * q[2])
+    cosy_cosp = 1 - 2 * (q[2] * q[2] + q[3] * q[3])
+    yaw_z = math.atan2(siny_cosp, cosy_cosp)
+
+    return roll_x, pitch_y, yaw_z
+
+def from_euler_321(roll_x: float, pitch_y: float, yaw_z: float) -> QuaternionType:
+    cr = math.cos(roll_x * 0.5)
+    sr = math.sin(roll_x * 0.5)
+    cp = math.cos(pitch_y * 0.5)
+    sp = math.sin(pitch_y * 0.5)
+    cy = math.cos(yaw_z * 0.5)
+    sy = math.sin(yaw_z * 0.5)
+    return (cr * cp * cy + sr * sp * sy,
+            sr * cp * cy - cr * sp * sy,
+            cr * sp * cy + sr * cp * sy,
+            cr * cp * sy - sr * sp * cy)
+
+def euler_321_to_123(xyz):
+    c1, c2, c3 = tuple(math.cos(i) for i in xyz)
+    s1, s2, s3 = tuple(math.sin(i) for i in xyz)
+    m = ((c2*c3, -c2*s3, s2),
+         (c1*s3+c3*s1*s2, c1*c3-s1*s2*s3, -c2*s1),
+         (s1*s3-c1*c3*s2, c3*s1+c1*s2*s3, c1*c2))
+    a1 = math.atan2(m[1][0], m[0][0])  # z
+    a2 = math.asin(-m[2][0])           # y
+    a3 = math.atan2(m[2][1], m[2][2])  # x?
+    return a3, a2, a1
+def euler_123_to_321(xyz):
+    c1, c2, c3 = tuple(math.cos(i) for i in xyz)
+    s1, s2, s3 = tuple(math.sin(i) for i in xyz)
+    m = ((c1*c2, c1*s2*s3-c3*s1, s1*s3 + c1*c3*s2),
+         (c2*s1, c1*c3+s1*s2*s3, c3*s1*s2-c1*s3),
+         (-s2, c2*s3, c2*c3))
+    a1 = math.atan2(-m[1][2], m[2][2])  # x
+    a2 = math.asin(m[0][2])             # y
+    a3 = math.atan2(-m[0][1], m[0][0])  # z
+    return a3, a2, a1
+
+
+if __name__ == "__main__":
+    angles = [(0, 0, 0),
+              (math.pi, 1, 2)]
+    for a in angles:
+        print(f"{a=} -> q={from_euler(*a)} -> e(q)={to_euler(from_euler(*a))}")
